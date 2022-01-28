64edad34be74977d5476f7269b5876140d813faa
Nimeshikasri <nimeshikasri.k@gmail.com>
2022-01-28 12:00:01 +0530

    test for add and multiply

---
+++ app/src/main/java/com/example/firstapp/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.firstapp;
+
+public class Calculator {
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+}
