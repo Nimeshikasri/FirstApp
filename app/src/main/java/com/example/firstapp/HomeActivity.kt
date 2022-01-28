605e4dcb27e4035e9a7a584f59577d157b87e535
Nimeshikasri <nimeshikasri.k@gmail.com>
2022-01-28 09:14:45 +0530

    start activity for result

--- app/src/main/java/com/example/firstapp/HomeActivity.kt
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,9 @@ import android.widget.TextView
 import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var tvHome: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -17,11 +21,31 @@ class HomeActivity : AppCompatActivity() {
         tvHome.text = name
     }
 
-    fun handleClick(view: View) {
-        var intent: Intent    //kotlin says you can't have a variable as null
-        intent = Intent(this,MainActivity::class.java)  //no need to give the word new
-        intent.putExtra("ds","divyang sharma")
-        startActivity(intent)
+    fun handleClick(view: android.view.View) {
+        when(view.id){
+            R.id.btnSend -> {  startDialer() }
+            R.id.btnMain -> {startMain() }
+        }
+    }
 
+    private fun startDialer() {
+        var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:21345678"))
+        startActivity(dialIntent)
+    }
+
+    private fun startMain() {
+        var intent: Intent    //kotlin says you can't have a variable as null
+        intent = Intent(this, MainActivity::class.java)  //no need to give the word new
+        intent.putExtra("ds", "divyang sharma")
+        startActivityForResult(intent,123)
+    }
+
+
+    //result will arrive here -- photo, contact
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == 123){ //RESULT_OK means its good to consume, 123 -- data being returned is of type contact
+            tvHome.text = data?.extras?.getString("con")
+        }
     }
 }
--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,8 +22,45 @@ public class MainActivity extends AppCompatActivity {
         mTextView.setText(name);
     }
 
-    public void handleClick(View view) {
 
+    public void handleClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                startHome();
+                break;
+            case R.id.btnCancel:
+                createAlarm("bajaj",14,29);
+                break;
+            case R.id.btnFinish:
+                sendContact();
+                break;
+        }
+    }
+
+    private void sendContact() {
+        //get the contact from edittext
+        EditText nameEditText = findViewById(R.id.etName);
+        String contact = nameEditText.getText().toString();
+        //put the contact in intent
+        Intent cIntent = new Intent();
+        cIntent.putExtra("con",contact);
+        //set the result
+        setResult(RESULT_OK,cIntent);
+        //close this activity
+        finish();
+    }
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void startHome() {
         Toast.makeText(this, "loggin in", Toast.LENGTH_SHORT).show();
         Intent mIntent;
         mIntent  = new Intent(MainActivity.this,HomeActivity.class);
