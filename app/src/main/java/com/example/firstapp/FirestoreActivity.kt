6ce337e2b8cdab78c7c92c257d488fa4332418c5
Nimeshikasri <nimeshikasri.k@gmail.com>
2022-01-31 10:11:37 +0530

    content provider sms

---
+++ app/src/main/java/com/example/firstapp/FirestoreActivity.kt
@@ -0,0 +1,14 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class FirestoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firestore)
+    }
+
+    fun firestoreHandler(view: View) {}
+}
